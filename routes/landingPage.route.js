0ed5deedd7a73fbadf79c8da57d325c817355d04
AryanSingh2006 <aryansingh18102006@gmail.com>
2025-07-16 18:57:18 +0530

    landing page route

---
+++ routes/landingPage.route.js
@@ -0,0 +1,8 @@
+import express from "express";
+import renderLandingPage from "../controller/landingPage.controller.js";
+
+const router = express.Router();
+
+router.get('/', renderLandingPage);
+
+export default router;
