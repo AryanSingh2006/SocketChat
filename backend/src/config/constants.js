ba0d3072db24655b6328637186f75e4f58262696
AryanSingh2006 <aryansingh18102006@gmail.com>
2025-11-01 15:16:48 +0530

    Added Cloudinary Constants

--- backend/src/config/constants.js
+++ backend/src/config/constants.js
@@ -7,3 +7,6 @@ export const DB_URI = process.env.DB_URI;
 export const JWT_ACCESS_KEY = process.env.JWT_ACCESS_KEY;
 export const JWT_ACCESS_EXPIRY = process.env.JWT_ACCESS_EXPIRY;
 export const NODE_ENV = process.env.NODE_ENV
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET
