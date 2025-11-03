76b91deb35e4d02e449ccf0e11bd4d93f62ba2c6
AryanSingh2006 <aryansingh18102006@gmail.com>
2025-11-03 20:47:08 +0530

    .

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,4 +1,5 @@
 import { create } from "zustand"
+import { axiosInstance } from "../lib/axios"
 
 export const useAuthStore = create((set) => ({
   authUser: null,
@@ -7,4 +8,19 @@ export const useAuthStore = create((set) => ({
   isUpdatingProfile: false,
 
   isCheckingAuth: true,
+
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get('/auth/check');
+
+      set({ authUser: res.data });
+    } catch (error) {
+      console.log("message: ", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false })
+    }
+  },
+
+  signup: async (data) => { }
 }))
